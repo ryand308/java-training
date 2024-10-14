a0e690281bb2ee4c9df3618c0a17a0277039d8b2
curtis Kai <113866145+ryand308@users.noreply.github.com>
2024-10-14 02:40:20 +0000

    onlineTest

---
+++ Trycatch.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class TryCatch {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        Integer[] x = {11, 23, 41, 56, 97};
+        for(Integer i: x) {
+            list.add(i);
+        }
+        try {
+            // forEach 也可以使用。為故意製造出 ArrayOutOfBoundsException
+            for(int i = 0; i <= list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+        }catch (ArrayOutOfBoundsException e) {
+            e.getMessage();
+            System.out.println("出現未定義行為。");
+        }finally {
+            System.out.println("test print finally local.");
+        }
+    }
+}
